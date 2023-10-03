7508350df5e51a9d23eb7003c3163f4e6b27c435
Kappa56799 <Kacper200657@hotmail.com>
2023-10-03 09:27:03 +0100

    SoftwareEng

---
+++ OOP/lab1/lab1.py
@@ -0,0 +1,44 @@
+#Question 10
+'''
+from math import pi
+l, w, h = 4, 2, 3
+area = l * w * h
+print("The area of a rectangle of length", l, "width", w,"height",h,"is", area)
+'''
+
+#Question 11
+'''
+input = int(input("Enter a number: "))
+sum = ((((input + 2) * 3) - 6) / 3)
+print(sum)
+'''
+
+#Question 12
+'''
+my_int = 5
+my_int = my_int += 3
+print(my_int)
+'''
+
+#Question 13
+'''
+my_var1 = 7.0
+my_var2 = 5
+print(my_var1 % my_var2)
+'''
+
+#Question 14
+'''
+input = input("Enter Something!")
+int(input)
+float(input)
+list(input)
+tuple(input)
+str(input)
+'''
+
+#Question 15
+'''
+output = lambda input: input % 2
+print(output(int(input("Enter A number:"))))
+'''
---
+++ OOP/lab2/lab2.py
@@ -0,0 +1,26 @@
+#Question 1
+'''
+input = int(input("Enter a number: "))
+if input >= 40:
+    print("Pass")
+else:
+    print("Fail")
+'''
+
+#Question 2
+'''
+num = int(input("Enter a number: "))
+num1 = int(input("Enter a number: "))
+
+if num > num1:
+    print("The first number is greater than the second number")
+
+if num < num1:
+    print("The first number is less than the second number")
+
+else:
+    print("The first number is equal to the second number")
+    '''
+
+#Question 3
+
